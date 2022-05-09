929d4ab6ccdcf0942110e5db9c515cb12544137f
AlejandroMM05 <99497639+AlejandroMM05@users.noreply.github.com>
2022-05-09 03:56:07 +0200

    Funciones de movimiento y prueba de eleccion de posicion

--- juego.c
+++ juego.c
@@ -67,168 +67,235 @@ void Dificil() {
     resultadosLB();
 }
 
-void juego(FILE *map) {
+void juego(FILE *map) 
+{
     //[numero de laberinto][fila del laberinto][columna del laberinto]
     char mapas[30][200][200];
 
     //utilizaremos la i para el numero de laberinto
     //la j para la fila del laberinto y la k para la columna del laberinto
     int i, j, k;
-    dosDatos medidas[30], posicion[30], meta[30];
+    dosDatos medidas[30];
     
-    for (i = 0; i < 30; i++) {
+    for (i = 0; i < 30; i++) 
+    {
         //obetenemos las dimensines del laberinto y las almacenamos
         //en una estructura
         fscanf(map, "%d %d", &medidas[i].x, &medidas[i].y);
         //obtenemos el laberinto escaneando por filas
-        for (j = 0; j < medidas[i].x; j++) {
+        for (j = 0; j < medidas[i].x; j++) 
+        {
             fscanf(map, "%s", mapas[i][j]);
             //va escaneando por cada elemento de la matriz
             //buscando "o" para sustituirlos por espacios.
-            for (k = 0; k < medidas[i].y; k++) {
-                if (mapas[i][j][k] == 'o') {
+            for (k = 0; k < medidas[i].y; k++) 
+            {
+                if (mapas[i][j][k] == 'o') 
+                {
                     mapas[i][j][k] = ' ';
-                }
-                //escaneo del punto de inicio
-                //no va a ser necesario cuando se ponga el 
-                //sistema para que el jugador ponga el punto de inicio
-                if (mapas[i][j][k] == 'X') {
-                    posicion[i].x = j;
-                    posicion[i].y = k;
-                }
-                //escaneo de la meta
-                if (mapas[i][j][k] == 'M') {
-                    meta[i].x = j;
-                    meta[i].y = k;
                 }
             }
         }
     }
-
+    dosDatos salida;
     //a arreglar: la n tiene que ser random para elegir el mapa
-    int n = 0;
-    char input;
-    //a arreglar: la posicion no tiene que estar determinada por el mapa
-    //sino que tiene la tiene que dar el jugador.
+    int mapa_jugable;
+    //Provisional
+    mapa_jugable=0;
+    //Para que funcione con los demas mapas hay que borrar las X en todos los mapas
 
-    //declaraciones de variables que se utilizan
-    dosDatos posicionactual, render;
-    posicionactual.x = posicion[n].x;
-    posicionactual.y = posicion[n].y;
-    int check = 1;
-    char izquierda, derecha, arriba, abajo;
-
-    while (check == 1) {
+    //Funcion para definir cordenadas de jugador
+    decidir_posicion(mapas, mapa_jugable);
+    salida=analizar_posicion(mapas, medidas, mapa_jugable);
+    do
+    {
+        imprimir_area(mapas, medidas, mapa_jugable);
+        moverse(mapas, medidas, mapa_jugable);
         system("cls");
-        //renderizado del mapa
-        //si el jugador esta en una esquina se renderiza bien:
-        render.x = posicionactual.x;
-        render.y = posicionactual.y;
-        /* if (posicionactual.y < 12) {
-            render.y = 12;
-        }
-        if (posicionactual.x < 5) {
-            render.x = 5;
-        }
-        if (posicionactual.y > medidas[n].y - 12) {
-            render.y = medidas[n].y - 12;
-        }
-        if (posicionactual.x > medidas[n].x - 5) {
-            render.x = medidas[n].x - 5;
-        }
- */
-        //imprime el mapa en pantalla
-        for (i = render.x - 5; i < render.x + 5; i++) {
-            for (j = render.y - 12; j < render.y + 12; j++) {
-                printf("%c", mapas[n][i][j]);
+    } while (mapas[mapa_jugable][salida.x][salida.y]=='X');
+    system("cls");
+}
+
+void movimiento(char c[][200][200], dosDatos medidas[], int mapa_jugable)
+{
+    int i, j, k=0;
+    dosDatos jugador;
+    char up, down, left, right;
+    char tecla_mov;
+    //Busca donde se encuentra el jugador
+    jugador=analizar_posicion(c, medidas, mapa_jugable);
+    //determina lo que rodea al jugador
+    up=c[mapa_jugable][jugador.x-1][jugador.y];
+    down=c[mapa_jugable][jugador.x+1][jugador.y];
+    right=c[mapa_jugable][jugador.x][jugador.y+1];
+    left=c[mapa_jugable][jugador.x][jugador.y-1];
+    //elimina la posicion anterior del jugador
+    c[mapa_jugable][jugador.x][jugador.y]=' ';
+    do
+    {
+        fflush(stdin);
+        tecla_mov=getch();
+        switch (tecla_mov)
+        {
+        case('w'):
+            if (up=' '||up=='M')
+            {
+                jugador.x=jugador.x-1;
+                k=1;
             }
-            printf("\n");
+            else
+            {
+                k=0;
+            }
+            break;
+        case ('W'):
+            if (up=' '||up=='M')
+            {
+                jugador.x=jugador.x-1;
+                k=1;
+            }
+            else
+            {
+                k=0;
+            }
+            break;
+        case ('s'):
+            if (down=' '||down=='M')
+            {
+                jugador.x=jugador.x+1;
+                k=1;
+            }
+            else
+            {
+                k=0;
+            }
+            break;
+        case ('S'):
+            if (down=' '||down=='M')
+            {
+                jugador.x=jugador.x+1;
+                k=1;
+            }
+            else
+            {
+                k=0;
+            }
+            break;
+        case ('d'):
+            if (right=' '||right=='M')
+            {
+                jugador.y=jugador.y+1;
+                k=1;
+            }
+            else
+            {
+                k=0;
+            }
+            break;
+        case ('D'):
+            if (right=' '||right=='M')
+            {
+                jugador.y=jugador.y+1;
+                k=1;
+            }
+            else
+            {
+                k=0;
+            }
+            break;
+        case ('a'):
+            if (left=' '||left=='M')
+            {
+                jugador.y=jugador.y-1;
+                k=1;
+            }
+            else
+            {
+                k=0;
+            }
+            break;
+        case ('A'):
+            if (left=' '||left=='M')
+            {
+                jugador.y=jugador.y-1;
+                k=1;
+            }
+            else
+            {
+                k=0;
+            }
+            break;
+        default:
+            break;
         }
-        //coge todo lo que esta alrededor del jugador
-        arriba = mapas[n][posicionactual.x - 1][posicionactual.y];
-        izquierda = mapas[n][posicionactual.x][posicionactual.y - 1];
-        abajo = mapas[n][posicionactual.x + 1][posicionactual.y];
-        derecha = mapas[n][posicionactual.x][posicionactual.y + 1];
-        //borra la X anterior
-        mapas[n][posicionactual.x][posicionactual.y] = ' ';
-        
-        //movimiento
-        input = getch();
-        switch (input) {
-            case 'w':
-                if (arriba == ' ' || arriba == 'M') {
-                    posicionactual.x--;
-                } else {
-                    break;
-                }
-                break;
-            case 'W': 
-                if (arriba == ' ' || arriba == 'M') {
-                    posicionactual.x--;
-                } else {
-                    break;
-                }
-                break;
-            case 'a':
-                if (izquierda == ' ' || izquierda == 'M') {
-                    posicionactual.y--;
-                } else {
-                    break;
-                }
-                break;
-            case 'A':
-                if (izquierda == ' ' || izquierda == 'M') {
-                    posicionactual.y--;
-                } else {
-                    break;
-                }
-                break;
-            case 's':
-                if (abajo == ' ' || abajo == 'M') {
-                    posicionactual.x++;
-                } else {
-                    break;
-                }
-                break;
-            case 'S':
-                if (abajo == ' ' || abajo == 'M') {
-                    posicionactual.x++;
-                } else {
-                    break;
-                }
-                break;
-            case 'd':
-                if (derecha == ' ' || derecha == 'M') {
-                    posicionactual.y++;
-                } else {
-                    break;
-                }
-                break;
-            case 'D':
-                if (derecha == ' ' || derecha == 'M') {
-                    posicionactual.y++;
-                } else {
-                    break;
-                }
-                break;
-            
-            default:
-                break;
-        }
-        //Pone la X en el mapa de nuevo
-        mapas[n][posicionactual.x][posicionactual.y] = 'X';
-        //comprueba si el jugador ha llegado a la meta
-        if (posicionactual.x == meta[n].x) {
-            if (posicionactual.y == meta[n].y) {
-                system("cls");
-                check = 0;
+    } while (k==0);
+    c[mapa_jugable][jugador.x][jugador.y]='X';
+}
+dosDatos analizar_posicion(char c[][200][200], dosDatos medidas[], int mapa_jugable)
+{
+    dosDatos posicion_jugador;
+    int i, j, k;
+    for ( i = 0; i < medidas[mapa_jugable].x; i++)
+    {
+        for ( j = 0; j < medidas[mapa_jugable].y; j++)
+        {
+            if (c[mapa_jugable][i][j]=='X')
+            {
+                posicion_jugador.x=i;
+                posicion_jugador.y=j;
             }
         }
-        if (posicionactual.y == meta[n].y) {
-            if (posicionactual.x == meta[n].x) {
-                system("cls");
-                check = 0;
-            }
+    }
+    return posicion_jugador;
+}
+void decidir_posicion(char c[][200][200], int mapa_jugable)
+{
+    dosDatos jugador;
+    int k=0;
+    do
+    {
+        printf("Escribe una posicion de la cual empezar\n");
+        printf("Recuerda las dimensiones de los niveles\nFacil:25x25\nMedio:50x50\nDificil:100x100\n");
+        scanf("%i %i", &jugador.x, &jugador.y);
+        if (c[mapa_jugable][jugador.x][jugador.y]==' ')
+        {
+            c[mapa_jugable][jugador.x][jugador.y]='X';
+            k=1;
         }
+        else
+        {
+            k=0;
+            printf("Si no se borra este comando es porque la posicion coincide con una pared, por favor ingrese otra cordenada\n");
+            system("pause");
+            system("cls");
+        }
+    } while (k==0);
+}
+void imprimir_area(char c[][200][200], dosDatos medidas[], int mapa_jugable)
+{
+    int i, j, k;
+    dosDatos jugador, area;
+    jugador=analizar_posicion(c, medidas, mapa_jugable);
+    if (jugador.y<12){
+        area.y=12;
+    }
+    if (jugador.x<5){
+        area.x=5;
+    }
+    if (jugador.y>medidas[mapa_jugable].y-12){
+        area.y=medidas[mapa_jugable].y-12;
+    }
+    if (jugador.x>medidas[mapa_jugable].x-5){
+        area.x=medidas[mapa_jugable].x-5;
+    }
+    printf("\n\n\n\n\n");
+    for ( i = area.x-5; i < area.x+5; i++)
+    {
+        printf("\t\t\t\t\t\t");
+        for ( j = area.y-12; j < area.y+12; j++)
+        {
+            printf("%c ", c[mapa_jugable][i][j]);
+        }
+        printf("\n");
     }
 }
--- juego.h
+++ juego.h
@@ -1,3 +1,11 @@
+typedef struct 
+{
+    //fila x columna
+    //la x es el numero de fila
+    //la y es el numero de columna
+    int x, y;
+} dosDatos;
+
 //opcion facil
 void Facil();
 
@@ -9,3 +17,11 @@ void Dificil();
 
 //el juego en si
 void juego(FILE *map);
+
+//funciones en relacion a movimiento
+dosDatos analizar_posicion(char c[][200][200], dosDatos medidas[], int mapa_jugable);
+void decidir_posicion(char c[][200][200], int mapa_jugable);
+void moverse(char c[][200][200], dosDatos medidas[], int mapa_jugable);
+
+//funcion de muestra de mapa
+void imprimir_area(char c[][200][200], dosDatos medidas[], int mapa_jugable);
