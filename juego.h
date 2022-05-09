c2c59424a412d113431750944cdbe67191f094b9
AlejandroMM05 <99497639+AlejandroMM05@users.noreply.github.com>
2022-05-09 03:59:46 +0200

    Update juego.h

--- juego.h
+++ juego.h
@@ -10,6 +10,11 @@ void Dificil();
 //el juego en si
 void juego(FILE *map);
 
+typedef struct 
+{
+    int x, y;
+}dosDatos;
+
 //funciones en relacion a movimiento
 dosDatos analizar_posicion(char c[][200][200], dosDatos medidas[], int mapa_jugable);
 void decidir_posicion(char c[][200][200], int mapa_jugable);
