e256e3deadd166745c43745a1cbbf51d146ccbd1
AlejandroMM05 <99497639+AlejandroMM05@users.noreply.github.com>
2022-05-09 04:03:21 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
-
-typedef struct 
-{
-    //fila x columna
-    int x, y;
-} dosDatos;
-
 #include "pantallas.h"
 #include "acciones.h"
 #include "juego.h"
