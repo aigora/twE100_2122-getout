adc58fbffe4f62add41721f0e352a8d3860a92e6
AlejandroMM05 <99497639+AlejandroMM05@users.noreply.github.com>
2022-05-13 18:58:13 +0200

    Editor de niveles añadido

---
+++ editor.c
@@ -0,0 +1,282 @@
+#pragma once
+#include <stdio.h>
+#include <conio.h>
+#include "editor.h"
+
+void editor()
+{
+    FILE *map;
+    int i, j, detectar, k=0;
+    char save, editar, pared, mapa[50][50];
+    dosDatos p, medidas;
+    p.x=1;
+    p.y=1;
+    do
+    {
+        printf("Determine la dimension del laberinto(maximo 50x50)\n");
+        scanf("%i %i", &medidas.x, &medidas.y);
+        if (medidas.x>0&&medidas.x<=50)
+        {
+            if (medidas.y>0&&medidas.y<=50)
+            {
+                k=1;
+            }
+        }
+        else
+        {
+            printf("Escribe un laberinto con las caracteristicas indicadas\n");
+            k=0;
+        }
+    } while (k==0);
+    printf("Cuando termines presiona G(solo usa mayusculas con la G)\n");
+    for ( i = 0; i < medidas.x; i++)
+    {
+        for ( j = 0; j < medidas.y; j++)
+        {
+            mapa[i][j]='_';
+        }      
+    }
+    for ( i = 0; i < medidas.x; i++)
+    {
+        mapa[i][0]='#';
+        mapa[i][medidas.y-1]='#';
+    }
+    for ( j = 0; j < medidas.y; j++)
+    {
+        mapa[0][j]='#';
+        mapa[medidas.x-1][j]='#';
+    }
+    system("pause");
+    system("cls");
+    do
+    {
+        imprimir_lab(mapa,medidas, p);
+        printf("mueve cursor(w,a,s,d)\n");
+        printf("presiona segun lo que quieras colocar:\npared(p)\nespacio(o)\nsalida(m)\nGuardar(G)");
+        fflush(stdin);
+        pared=getch();
+        if (pared=='w'||pared=='d'||pared=='a'||pared=='s')
+        {
+            p=mover_cursor(mapa,medidas, p, pared);
+        }
+        if (pared=='p'||pared=='o'||pared=='m')
+        {
+            editar_lab(mapa, medidas, p, pared);
+        }
+        /* if (pared=='o')
+        {
+            editar_lab(c, a, b, p, pared);
+        }
+        if (pared=='m')
+        {
+            editar_lab(c, a, b, p, pared);
+        } */
+        if (pared=='G'||pared=='g')
+        {
+            detectar=detectar_salida(mapa, medidas, p);
+            if (detectar==1)
+            {
+                save=guardar();
+            }
+            else
+            {
+                imprimir_lab(mapa, medidas, p);
+                printf("\nNo has a%cadido una salida\n", 164);
+                system("pause");
+            }
+        }
+        
+    } while (save!='G');
+    for ( i = 0; i < medidas.x; i++)
+    {
+        for ( j = 0; j < medidas.y; j++)
+        {
+            if (mapa[i][j]=='_')
+            {
+                mapa[i][j]='o';
+            }
+        }
+    }
+    map=fopen("Niveles_guardados.txt", "a");
+    fprintf(map, "%i %i\n", medidas.x, medidas.y);
+    for ( i = 0; i < medidas.x; i++)
+    {
+        for ( j = 0; j < medidas.y; j++)
+        {
+            fprintf(map, "%c", mapa[i][j]);
+        }
+        fprintf(map, "\n");
+    }
+    fclose(map);
+}
+void imprimir_lab(char mapa[50][50], dosDatos medidas, dosDatos posicion)
+{
+    int i, j;
+    system("cls");
+    printf("\n\n\n\n\n");
+    for ( i = 0; i < medidas.x; i++)
+    {
+        printf("\t\t\t\t\t");
+        for ( j = 0; j < medidas.y; j++)
+        {
+            if (i==posicion.x&&j==posicion.y)
+            {
+                if (mapa[i][j]=='#')
+                {
+                    printf("\x1b[33m");
+                    printf("%c%c", 219, 219);
+                    printf("\x1b[0m");
+                }
+                else
+                {
+                    printf("\x1b[33m");
+                    printf("%c ", mapa[i][j]);
+                    printf("\x1b[0m");
+                }
+            }
+            else
+            {
+                if (mapa[i][j]=='#')
+                {
+                    printf("%c%c", 219, 219);
+                }
+                else
+                {
+                    printf("%c ", mapa[i][j]);
+                }
+            }
+        }
+        printf("\n");
+    }
+}
+void editar_lab(char mapa[50][50], dosDatos medidas, dosDatos posicion, char caso)
+{
+    int permiso;
+    if (caso=='p')
+    {
+        mapa[posicion.x][posicion.y]='#';
+    }
+    if (caso=='o'||caso=='w'||caso=='a'||caso=='s'||caso=='d')
+    {
+        mapa[posicion.x][posicion.y]='_';
+    }
+    if (caso=='m')
+    {
+        permiso=detectar_salida(mapa, medidas, posicion);
+        if (permiso==1)
+        {
+            printf("\nYa existe una salida\n");
+            system("pause");
+            mapa[posicion.x][posicion.y]='_';
+        }
+        if (permiso==0)
+        {
+            mapa[posicion.x][posicion.y]='M';
+        }   
+    }
+}
+dosDatos mover_cursor(char mapa[50][50], dosDatos medidas, dosDatos posicion, char caso)
+{
+    int k=1;
+    do{
+        switch (caso)
+        {
+            case ('w'):
+                if (posicion.x<=medidas.x-1&&posicion.x>=0)
+                {
+                    if (posicion.x==0)
+                    {
+                        k=1;
+                    }
+                    else
+                    {
+                        posicion.x=posicion.x-1;
+                        k=0;
+                    }
+                }
+                else
+                k=1;
+                break;
+            case ('s'):
+                if (posicion.x<=medidas.x-1&&posicion.x>=0)
+                {
+                    if (posicion.x==medidas.x-1)
+                    {
+                        k=1;
+                    }
+                    else
+                    {
+                        posicion.x=posicion.x+1;
+                        k=0;
+                    }
+                }
+                else
+                k=1;
+                break;
+            case ('d'):
+                if (posicion.y<=medidas.y-1&&posicion.y>=0)
+                {
+                    if (posicion.y==medidas.y-1)
+                    {
+                        k=1;
+                    }
+                    else
+                    {
+                        posicion.y=posicion.y+1;
+                        k=0;
+                    }
+                }
+                else
+                k=1;
+                break;
+            case ('a'):
+                if (posicion.y<=medidas.y-1&&posicion.y>=0)
+                {
+                    if (posicion.y==0)
+                    {
+                        k=1;
+                    }
+                    else
+                    {
+                        posicion.y=posicion.y-1;
+                        k=0;
+                    }
+                }
+                else
+                k=1;
+                break;
+            default:
+                break;
+            }
+        }while(k==1);
+    return posicion;
+}
+char detectar_salida(char mapa[50][50], dosDatos medidas, dosDatos posicion)
+{
+    int i, j;
+    for ( i = 0; i < medidas.x; i++)
+    {
+        for ( j = 0; j < medidas.y; j++)
+        {
+            if (mapa[i][j]=='M')
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+char guardar()
+{
+    char aux;
+    printf("\nQuieres guardar?\nsi(y)\tno(n)");
+    aux=getch();
+    if (aux=='y')
+    {
+        return 'G';
+    }
+    else
+    {
+        return 'a';
+    }
+}
